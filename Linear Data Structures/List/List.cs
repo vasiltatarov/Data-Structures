7669fdd5dc214d7b48dd8a42a11c456aa0b33ecc
vasil tatarov <vasil.tatarov@abv.bg>
2020-09-04 23:51:01 +0300

    CustomList

---
+++ Linear Data Structures/List/List.cs
@@ -0,0 +1,161 @@
+﻿namespace Problem01.List
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class List<T> : IAbstractList<T>
+    {
+        private const int DEFAULT_CAPACITY = 4;
+        private T[] _items;
+
+        public List()
+            : this(DEFAULT_CAPACITY) {
+        }
+
+        public List(int capacity)
+        {
+            ValidateCapacity(capacity);
+
+            this._items = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+
+                return this._items[index];
+            }
+            set
+            {
+                ValidateIndex(index);
+
+                this._items[index] = value;
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            if (this._items.Length == this.Count)
+            {
+                Resize();
+            }
+
+            this._items[this.Count] = item;
+            this.Count++;
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this._items[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this._items[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            ValidateIndex(index);
+
+            if (this._items.Length == this.Count)
+            {
+                Resize();
+            }
+
+            for (int i = this.Count; i > index; i--)
+            {
+                this._items[i] = this._items[i - 1];
+            }
+
+            this._items[index] = item;
+            this.Count++;
+        }
+
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this._items[i].Equals(item))
+                {
+                    this.RemoveAt(i);
+                }
+            }
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            ValidateIndex(index);
+
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this._items[i] = this._items[i + 1];
+            }
+
+            this._items[this.Count - 1] = default;
+            this.Count--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this._items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() 
+            => this.GetEnumerator();
+
+        private void Resize()
+        {
+            var newArr = new T[this._items.Length * 2];
+
+            for (int i = 0; i < this._items.Length; i++)
+            {
+                newArr[i] = this._items[i];
+            }
+
+            this._items = newArr;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException(nameof(index));
+            }
+        }
+
+        private static void ValidateCapacity(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+            }
+        }
+    }
+}
