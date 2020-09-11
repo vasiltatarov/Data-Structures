bbc23c66db64698ee6f757894fd89761a8784314
vasil tatarov <vasil.tatarov@abv.bg>
2020-09-11 11:26:08 +0300

    DS

---
+++ Linear Data Structures/BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,58 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            if (string.IsNullOrEmpty(parentheses) || parentheses.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            var stack = new Stack<char>();
+
+            for (int i = 0; i < parentheses.Length; i++)
+            {
+                var currParentheses = parentheses[i];
+                char expectedParentheses = default;
+
+                if (currParentheses == '(' || currParentheses == '[' || currParentheses == '{')
+                {
+                    stack.Push(currParentheses);
+                    continue;
+                }
+                else  if (currParentheses == ')')
+                {
+                    expectedParentheses = '(';
+                }
+                else if (currParentheses == ']')
+                {
+                    expectedParentheses = '[';
+                }
+                else if (currParentheses == '}')
+                {
+                    expectedParentheses = '{';
+                }
+
+                if (stack.Pop() == expectedParentheses)
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
